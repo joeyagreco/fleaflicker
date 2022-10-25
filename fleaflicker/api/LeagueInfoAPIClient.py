53314a4b4b4846948471f00bddaa01d7eae5facf
jg <57115019+joeyagreco@users.noreply.github.com>
2022-10-25 11:03:37 -0500

    cleanup

--- fleaflicker/api/LeagueInfoAPIClient.py
+++ fleaflicker/api/LeagueInfoAPIClient.py
@@ -7,8 +7,7 @@ class LeagueInfoAPIClient(FleaflickerAPIClient):
     @classmethod
     def get_league_activity_feed(cls, *, sport: Sport = Sport.NFL, league_id: int, result_offset: int = None) -> dict:
         filters = [("sport", sport.name), ("league_id", league_id)]
-        if result_offset is not None:
-            filters.append(("result_offset", result_offset))
+        cls._add_filter_if_given("result_offset", result_offset, filters)
         url = f"{cls._BASE_URL}{cls._LEAGUE_ACTIVITY_ROUTE}"
         url = cls._add_filters(url, *filters)
         return cls._get(url=url)
